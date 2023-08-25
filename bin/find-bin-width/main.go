02e2acd0c2ad634f1bb78415044dfbe73c1583c3
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2023-08-24 23:32:53 -0400

    cleanup

--- bin/find-bin-width/main.go
+++ bin/find-bin-width/main.go
@@ -16,6 +16,7 @@ func main() {
 	var out string
 
 	switch dataType {
+
 	case xtype.DataTypeUndecided:
 		fallthrough
 	case xtype.DataTypeInteger:
@@ -23,18 +24,22 @@ func main() {
 		values = nil
 		out = bw.FindIntegerBinWidth(ints, true)
 		break
+
 	case xtype.DataTypeFloat:
 		floats := xtype.ToFloats(values)
 		values = nil
 		out = bw.FindFloatBinWidth(floats, true)
 		break
+
 	case xtype.DataTypeDate:
 		dates := xtype.ToDates(values)
 		values = nil
 		out = bw.FindDateBinWidth(dates, true)
 		break
+
 	case xtype.DataTypeBoolean:
 		log.Fatalln("boolean data types not yet implemented")
+
 	default:
 		log.Fatalln("unrecognized or mixed data types passed on stdin")
 	}
