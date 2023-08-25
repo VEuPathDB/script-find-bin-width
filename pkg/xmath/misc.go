17b48d46100dbe0f0da6397355d53eddbed62cac
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2023-08-25 17:35:09 -0400

    comments

--- pkg/xmath/misc.go
+++ pkg/xmath/misc.go
@@ -4,14 +4,17 @@ import (
 	"find-bin-width/pkg/xutil"
 )
 
+// Max returns the greater of two given values.
 func Max[V float64 | int64](a, b V) V {
 	return xutil.IfElse(a > b, a, b)
 }
 
+// Min returns the lesser of two given values.
 func Min[V float64 | int64](a, b V) V {
 	return xutil.IfElse(a < b, a, b)
 }
 
+// UniqueN returns the count of distinct values in a given set of values.
 func UniqueN[V float64 | int64](values []V) int {
 	hold := make(map[V]bool)
 
@@ -22,10 +25,13 @@ func UniqueN[V float64 | int64](values []V) int {
 	return len(hold)
 }
 
+// Diff subtracts the first given value from the second given value.
 func Diff[V float64 | int64](x, y V) V {
 	return y - x
 }
 
+// Range returns the minimum and maximum values for a given set of numeric
+// values.
 func Range[V float64 | int64](values []V) (V, V) {
 	mi := values[0]
 	ma := values[0]
