07c7979c17a5dc965f347f92bef90662889b2f2e
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2024-02-26 18:01:23 -0500

    NonZeroRound

--- pkg/stats/integers.go
+++ pkg/stats/integers.go
@@ -88,7 +88,7 @@ func intNumBinsToBinWidth(values []int64, numBins int) inb2bwResult {
 	return inb2bwResult{
 		min:      info.min,
 		max:      info.max,
-		binWidth: max(int64(math.Round(binWidth)), 1),
+		binWidth: max(int64(xmath.NonZeroRound(binWidth, 0)), 1),
 	}
 }
 
