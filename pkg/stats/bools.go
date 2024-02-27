3ad96913b6cc527c4c8fcf24a9bc111179d6faba
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2024-02-27 13:59:06 -0500

    clear out for pr

--- pkg/stats/bools.go
+++
@@ -1,7 +0,0 @@
-package stats
-
-func findBooleanBinWidth(_ []bool) Stats {
-	// According to the original R implementation, boolean inputs always result in
-	// an NA output.
-	return naStats(0)
-}
--- pkg/xstr/consts.go
+++
@@ -1,8 +0,0 @@
-package xstr
-
-const (
-	AsciiHorizontalTab  = 9
-	AsciiLineFeed       = 10
-	AsciiCarriageReturn = 13
-	AsciiSpace          = 32
-)
