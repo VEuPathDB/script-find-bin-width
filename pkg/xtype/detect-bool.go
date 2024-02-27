57bb62520ad5a879d66d4baff29a2300338f491f
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2024-02-27 16:25:31 -0500

    fix bool val detection bug

--- pkg/xtype/detect-bool.go
+++ pkg/xtype/detect-bool.go
@@ -8,15 +8,15 @@ func valueIsBool(value string) bool {
 	case 4:
 		return (value[0] == 't' || value[0] == 'T') &&
 			(value[1] == 'r' || value[1] == 'R') &&
-			(value[1] == 'u' || value[1] == 'U') &&
-			(value[1] == 'e' || value[1] == 'E')
+			(value[2] == 'u' || value[2] == 'U') &&
+			(value[3] == 'e' || value[3] == 'E')
 
 	case 5:
 		return (value[0] == 'f' || value[0] == 'F') &&
 			(value[1] == 'a' || value[1] == 'A') &&
-			(value[1] == 'l' || value[1] == 'L') &&
-			(value[1] == 's' || value[1] == 'S') &&
-			(value[1] == 'e' || value[1] == 'E')
+			(value[2] == 'l' || value[2] == 'L') &&
+			(value[3] == 's' || value[3] == 'S') &&
+			(value[4] == 'e' || value[4] == 'E')
 
 	case 1:
 		for i := 0; i < len(shortBools); i++ {
@@ -32,7 +32,7 @@ func valueIsBool(value string) bool {
 	case 3:
 		return (value[0] == 'y' || value[0] == 'Y') &&
 			(value[1] == 'e' || value[1] == 'E') &&
-			(value[1] == 's' || value[1] == 'S')
+			(value[2] == 's' || value[2] == 'S')
 
 	default:
 		return false
