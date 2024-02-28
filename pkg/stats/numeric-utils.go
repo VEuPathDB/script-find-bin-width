667753ac16a288036e995b2c1d19b1f0737d21a2
Danielle Callan <dcallan@upenn.edu>
2024-02-27 23:50:00 -0500

    some notes about binning algorithms as comments

--- pkg/stats/numeric-utils.go
+++ pkg/stats/numeric-utils.go
@@ -10,14 +10,19 @@ func findNumBins[V float64 | int64](values []V) int {
 	var numBins float64
 	setBins := false
 
+	// Freedman-Diaconis is our 'default' method of choice
+	// it works well for reasonably large, normally distributed data
+	// its also less sensitive to outliers than some alternatives
 	if len(values) > 200 {
 		numBins = xmath.FD(values)
 		setBins = true
 	}
 
+	// this metric should give us a sense for normalcy
 	skewness, _ := xmath.Moments_Skewness(values)
 	absSkew := math.Abs(skewness)
 
+	// these data cant be normal, and so should use Doane's instead
 	if absSkew > 0.5 {
 		n := float64(len(values))
 		se := math.Sqrt(6 * (n - 2) / ((n + 1) * (n + 3)))
@@ -26,6 +31,7 @@ func findNumBins[V float64 | int64](values []V) int {
 		setBins = true
 	}
 
+	// sturges is shown to work well for n < 200 w normal data 
 	if !setBins {
 		numBins = xmath.Sturges(values)
 	}
